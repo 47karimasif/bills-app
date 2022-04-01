c960c6c99ef5741fb856377ba572a53117c94ae5
47karimasif <karimasif98@gmail.com>
2022-04-01 12:36:59 +0530

    loader integrated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/home";
 import NotFound from "../src/pages/notFound";
@@ -10,19 +10,29 @@ import Bills from "./pages/bills";
 import BillsContextProvider from "./context/billsContext";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
   return (
-    <BillsContextProvider>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:id" element={<Bills />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </BillsContextProvider>
+    !loading && (
+      <BillsContextProvider>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/:id" element={<Bills />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </BillsContextProvider>
+    )
   );
 };
 
